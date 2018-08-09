418ff1b44db8e79f04f5fb5bc17a4079c13544b5
Guilliam Xavier <guilliamxavier@users.noreply.github.com>
2018-08-09 14:34:41 +0200

    correct arguments to DOMDocument constructor (version, encoding) in test

--- tests/Doctrine/Tests/ORM/Mapping/XmlMappingDriverTest.php
+++ tests/Doctrine/Tests/ORM/Mapping/XmlMappingDriverTest.php
@@ -171,7 +171,7 @@ class XmlMappingDriverTest extends AbstractMappingDriverTest
     public function testValidateXmlSchema($xmlMappingFile) : void
     {
         $xsdSchemaFile = __DIR__ . '/../../../../../doctrine-mapping.xsd';
-        $dom           = new \DOMDocument('UTF-8');
+        $dom           = new \DOMDocument('1.0', 'UTF-8');
 
         $dom->load($xmlMappingFile);
 
